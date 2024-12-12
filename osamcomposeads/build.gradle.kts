5780f68e0944ad2181276518363683f045a73dcd
@Osama7869 <osamammuzaffar1122@gmail.com>
2024-12-12 12:41:08 +0500

    Testing 0.0.13

--- osamcomposeads/build.gradle.kts
+++ osamcomposeads/build.gradle.kts
@@ -67,7 +67,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.Osama-Muzaffar"
             artifactId = "osamcomposeads"
-            version = "1.0.0"
+            version = "1.0.1"
 
             afterEvaluate {
                 from(components["release"])
